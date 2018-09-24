17be2082b4e78ef9c5ddd18855ee9472ab00cd4b
KrishnaMadhuri12 <kompella.km@gmail.com>
2018-09-24 17:18:20 -0500

    percipitationCount

---
+++ WeatherApp/src/main/java/com/weather/report/september/PrecipitationCount.java
@@ -0,0 +1,32 @@
+package com.weather.report.september;
+
+/**
+ * 
+ * @author Krishna 
+ *
+ */
+public class PrecipitationCount {
+	Double count;
+	String startDate;
+	String endData;
+	
+	public Double getCount() {
+		return count;
+	}
+	public void setCount(Double count) {
+		this.count = count;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getEndData() {
+		return endData;
+	}
+	public void setEndData(String endData) {
+		this.endData = endData;
+	}
+	
+}
